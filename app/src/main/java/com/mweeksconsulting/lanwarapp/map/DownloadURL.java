bb33cdf9a032aa57a3e3de513fe3fda843c3f7ac
arshdeep144 <arshdeep.benipal@uoit.net>
2017-12-15 03:03:46 -0500

    adding maps to Arshdeep Branch

---
+++ app/src/main/java/com/mweeksconsulting/lanwarapp/map/DownloadURL.java
@@ -0,0 +1,55 @@
+package com.mweeksconsulting.lanwarapp.map;
+
+/**
+ * Created by Arshdeep on 2017-12-14.
+ */
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DownloadURL {
+    //Reads the markers from google maps website and returns it as a json
+    public String readUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strUrl);
+
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Connecting to url
+            urlConnection.connect();
+
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            data = sb.toString();
+            Log.d("downloadUrl", data.toString());
+            br.close();
+
+        } catch (Exception e) {
+            Log.d("Exception", e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+}
